6d5bffedeeffb7083acf089e141b750faf82c7bd
Hannah <Hannah@hallos-MacBook-Air.local>
2020-04-23 15:09:37 +0200

    variables added

--- Bookings.java
+++ Bookings.java
@@ -1,4 +1,11 @@
 
 public class Bookings {
 
+    int bookingId;
+    int dateOfBooking;
+    String status; // or int status if every status has a number: e.g. 1 = cancelled, etc...
+
+    public static void main (String[] args){
+
+    }
 }
--- Events.java
+++ Events.java
@@ -1,4 +1,18 @@
 
 public class Events {
 
+
+    int eventId;
+    String eventName;
+    String description;
+    String category;
+    int date;
+    int time;
+    String location;
+    int roomNumber;
+    String place;
+    boolean bookingRequired;
+
+    public static void main (String[] args){}
+    
 }

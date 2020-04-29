82c9fe209d46df3cbda45dfe203de5ebfb56fc9a
Cornelia11290 <57717846+Cornelia11290@users.noreply.github.com>
2020-04-29 11:54:23 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.txtBoxPass = new System.Windows.Forms.TextBox();
+            this.lblStID = new System.Windows.Forms.Label();
+            this.txtBoxUser = new System.Windows.Forms.TextBox();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxPassR = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.txtBoxConfPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBoxStFirstName = new System.Windows.Forms.TextBox();
+            this.txtBoxStLastName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(46, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(494, 316);
+            this.tabControl1.TabIndex = 5;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            this.tabControl1.Click += new System.EventHandler(this.tabControl1_Click);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lblPass);
+            this.tabPage1.Controls.Add(this.txtBoxPass);
+            this.tabPage1.Controls.Add(this.lblStID);
+            this.tabPage1.Controls.Add(this.txtBoxUser);
+            this.tabPage1.Controls.Add(this.btnLogIn);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(486, 290);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Login";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.txtBoxStLastName);
+            this.tabPage2.Controls.Add(this.txtBoxStFirstName);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.txtBoxConfPass);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.txtBoxPassR);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.btnRegister);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(486, 290);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Register";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(67, 84);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(53, 13);
+            this.lblPass.TabIndex = 9;
+            this.lblPass.Text = "Password";
+            // 
+            // txtBoxPass
+            // 
+            this.txtBoxPass.Location = new System.Drawing.Point(126, 77);
+            this.txtBoxPass.Name = "txtBoxPass";
+            this.txtBoxPass.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxPass.TabIndex = 8;
+            // 
+            // lblStID
+            // 
+            this.lblStID.AutoSize = true;
+            this.lblStID.Location = new System.Drawing.Point(62, 49);
+            this.lblStID.Name = "lblStID";
+            this.lblStID.Size = new System.Drawing.Size(58, 13);
+            this.lblStID.TabIndex = 7;
+            this.lblStID.Text = "Student ID";
+            // 
+            // txtBoxUser
+            // 
+            this.txtBoxUser.Location = new System.Drawing.Point(126, 42);
+            this.txtBoxUser.Name = "txtBoxUser";
+            this.txtBoxUser.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxUser.TabIndex = 6;
+            this.txtBoxUser.TextChanged += new System.EventHandler(this.txtBoxUser_TextChanged);
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Location = new System.Drawing.Point(139, 129);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(75, 23);
+            this.btnLogIn.TabIndex = 5;
+            this.btnLogIn.Text = "Log in ";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Password";
+            // 
+            // txtBoxPassR
+            // 
+            this.txtBoxPassR.Location = new System.Drawing.Point(169, 71);
+            this.txtBoxPassR.Name = "txtBoxPassR";
+            this.txtBoxPassR.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxPassR.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Student ID";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(169, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(183, 212);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnRegister.TabIndex = 10;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // txtBoxConfPass
+            // 
+            this.txtBoxConfPass.Location = new System.Drawing.Point(169, 107);
+            this.txtBoxConfPass.Name = "txtBoxConfPass";
+            this.txtBoxConfPass.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxConfPass.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Confirm Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Student First Name";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtBoxStFirstName
+            // 
+            this.txtBoxStFirstName.Location = new System.Drawing.Point(169, 145);
+            this.txtBoxStFirstName.Name = "txtBoxStFirstName";
+            this.txtBoxStFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxStFirstName.TabIndex = 18;
+            this.txtBoxStFirstName.TextChanged += new System.EventHandler(this.txtBoxStName_TextChanged);
+            // 
+            // txtBoxStLastName
+            // 
+            this.txtBoxStLastName.Location = new System.Drawing.Point(169, 181);
+            this.txtBoxStLastName.Name = "txtBoxStLastName";
+            this.txtBoxStLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxStLastName.TabIndex = 19;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(65, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Student Last Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 325);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.TextBox txtBoxPass;
+        private System.Windows.Forms.Label lblStID;
+        private System.Windows.Forms.TextBox txtBoxUser;
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxConfPass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxPassR;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.TextBox txtBoxStFirstName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBoxStLastName;
+    }
+}
+

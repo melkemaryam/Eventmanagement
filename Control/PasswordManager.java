e1f628513afdfd803a33cac11fcee1e4e96594e4
Hannah <Hannah@hallos-MacBook-Air.local>
2020-04-30 18:43:16 +0200

    small things

--- Control/PasswordManager.java
+++ Control/PasswordManager.java
@@ -1,17 +1,15 @@
 package Control;
 
 import java.math.BigInteger;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.MessageDigest;
 
 public class PasswordManager {
 
     public static String hashingPassword(String passToHash) throws NoSuchAlgorithmException {
 
         MessageDigest messageDig = MessageDigest.getInstance("SHA-256");
-
         byte[] messageDigest = messageDig.digest(passToHash.getBytes());
-
         BigInteger num = new BigInteger(1, messageDigest);
 
         StringBuilder strBuilder = new StringBuilder();

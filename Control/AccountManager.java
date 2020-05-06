4d45e63b7b6b80c2270474728e89a42dc28e1ad5
Hannah <Hannah@hallos-MacBook-Air.local>
2020-05-06 17:55:20 +0200

    start for login()

--- Control/AccountManager.java
+++ Control/AccountManager.java
@@ -1,5 +1,27 @@
 package Control;
 
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class AccountManager {
+
+    private static final Logger LOGGER = Logger.getLogger(DBManager.class.getName() );
+
+	private static final DBManager DATABASE = new DBManager();	
+
+    private static int id = 0;
+    
+    public AccountManager(){
+		//off
+		LOGGER.setLevel(Level.ALL);
+    }
+    
+    public static int logIn(int id, char[] password) {
+
+        return -1;
+    }
+
+
 
 }
--- Control/DBManager.java
+++ Control/DBManager.java
@@ -1,4 +1,4 @@
-package Control
+package Control;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
